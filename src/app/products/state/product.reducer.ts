b3864d9a70cf9656d351194827fdba234c5f0c1d
NGS\jovenson.omagap <jovenson.omagap@nowcom.com.ph>
2018-10-05 16:56:33 +0800

    deborah

---
+++ src/app/products/state/product.reducer.ts
@@ -0,0 +1,38 @@
+import { Product } from '../product';
+import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+
+export interface State extends fromRoot.State {
+    products: ProductState;
+}
+
+export interface ProductState {
+    showProductCode: boolean;
+    currentProduct: Product;
+    products: Product[];
+}
+
+const initialState: ProductState = {
+    showProductCode: true,
+    currentProduct: null,
+    products: []
+};
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+    getProductFeatureState,
+    state => state.showProductCode
+);
+
+export function reducer(state = initialState, action): ProductState {
+    switch (action.type) {
+        case 'TOGGLE_PRODUCT_CODE':
+            return {
+                ...state,
+                showProductCode: action.payload
+            };
+        default:
+            return state;
+    }
+}
